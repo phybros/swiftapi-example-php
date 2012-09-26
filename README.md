e33b718a14d37edeec6a2b1199723eeaaad63717
Will Warren <willdavidwarren@gmail.com>
2012-09-26 18:23:27 -0400

    Created repo and added the example project

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ swiftapi-example-php
 ====================
 
 An example client program for SwiftApi written in PHP.
+
+Requirements
+----
+
+* PHP 5.0+
+
+Instructions
+----
+Just drop all files into a web-accessible directory on your development machine.
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+	// change this to wherever you put the thrift library
+	$GLOBALS['THRIFT_ROOT'] = dirname(__FILE__) . "/thrift";
+	
+	// you have to put your gen-php code into the packages folder
+	$swiftroot = $GLOBALS['THRIFT_ROOT'] . "/packages";
+	
+	//swiftapi includes
+	require_once $swiftroot . "/SwiftApi/SwiftApi.php";
+	require_once $swiftroot . "/SwiftApi/SwiftApi_types.php";
+	require_once $swiftroot . "/Errors/Errors_types.php";
+	
+	//thrift includes
+	require_once $GLOBALS['THRIFT_ROOT'] . "/transport/TSocket.php";
+	require_once $GLOBALS['THRIFT_ROOT'] . "/transport/TFramedTransport.php";
+	require_once $GLOBALS['THRIFT_ROOT'] . "/protocol/TBinaryProtocol.php";
+
+	// Create an authString!
+	function getAuthString($methodName) {
+		$username = "admin";
+		$password = "password";
+		$salt = "saltines";
+	
+		$toHash = $username . $methodName . $password . $salt;
+		
+		return hash("sha256", $toHash);
+	}
+	
+	if($_POST) {
+		echo "Opening connection...<br />";
+		//open a new connection to the server
+		$socket = new TSocket('your.bukkitserver.org', 21111);
+		
+		// note: you must use the TFramedTransport
+		// TBufferedTransport is not compatible with SwiftApi
+		$transport = new TFramedTransport($socket);
+		$protocol = new TBinaryProtocol($transport);
+
+		echo "Creating client...<br />";
+		
+		//create a new SwiftApiClient object
+		$client= new SwiftApiClient($protocol, $protocol);
+		$transport->open();
+		
+		echo "Server Version: " . $client->getServerVersion(getAuthString("getServerVersion"));		
+		
+		//close the connection
+		$transport->close();
+	}
+?>
+
+<form action="" method="post">
+	<input name="submit" type="submit" value="Get Server Version" />
+</form>
